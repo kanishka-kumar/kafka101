82cad1784edb126d84a24fe456946256d14a031d
kanishka-kumar <kanishkakumar23@gmail.com>
2025-05-27 17:57:39 +0530

    Utilizing Wikimedia Stream for KafkaProducer

--- kafka-basics/src/main/java/com/kanishka/demo/kafka/ConsumerDemo.java
+++ kafka-basics/src/main/java/com/kanishka/demo/kafka/ConsumerDemo.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
--- kafka-basics/src/main/java/com/kanishka/demo/kafka/ProducerDemoWithKeys.java
+++ kafka-basics/src/main/java/com/kanishka/demo/kafka/ProducerDemoWithKeys.java
@@ -1,9 +1,8 @@
 package com.kanishka.demo.kafka;
 
-import org.apache.kafka.clients.producer.Callback;
+
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
